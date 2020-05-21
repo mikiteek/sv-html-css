fcf99896eaaac7de1d0739b8943000784c184674
mikiteek <mikiteek@gmail.com>
2020-05-21 21:51:20 +0300

    calculator complete

---
+++ calculator/scripts/calc.js
@@ -0,0 +1,112 @@
+calc.one.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.one.value;
+  },
+  false,
+);
+calc.two.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.two.value;
+  },
+  false,
+);
+calc.three.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.three.value;
+  },
+  false,
+);
+calc.four.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.four.value;
+  },
+  false,
+);
+calc.five.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.five.value;
+  },
+  false,
+);
+calc.six.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.six.value;
+  },
+  false,
+);
+calc.seven.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.seven.value;
+  },
+  false,
+);
+calc.eight.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.eight.value;
+  },
+  false,
+);
+calc.nine.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.nine.value;
+  },
+  false,
+);
+calc.devide.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.devide.value;
+  },
+  false,
+);
+calc.multiplication.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.multiplication.value;
+  },
+  false,
+);
+calc.minus.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.minus.value;
+  },
+  false,
+);
+calc.zero.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.zero.value;
+  },
+  false,
+);
+calc.cancel.addEventListener(
+  'click',
+  function () {
+    calc.result.value = '';
+  },
+  false,
+);
+calc.plus.addEventListener(
+  'click',
+  function () {
+    calc.result.value += calc.plus.value;
+  },
+  false,
+);
+calc.equal.addEventListener(
+  'click',
+  function () {
+    calc.result.value = eval(calc.result.value);
+  },
+  false,
+);
